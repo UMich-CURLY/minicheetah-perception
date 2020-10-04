9fe7669b948a88feea5b732651a62c29edceb2dc
cheetah-jetson <chienerh@eumich.com>
2020-10-04 17:31:03 -0400

    modify lcm2ros and added some scripts

--- scripts/build_submodules.sh
+++ scripts/build_submodules.sh
@@ -9,3 +9,11 @@ mkdir build
 cd build
 cmake ..
 make -j4
+
+cd ../../Cheetah-Software
+mkdir build
+cd build
+cmake .. -DMINI_CHEETAH_BUILD=TRUE
+./../scripts/make_types.sh
+make -j4
+
--- scripts/record_ros.sh
+++ scripts/record_ros.sh
@@ -1,1 +1,2 @@
+cd ../../data/
 rosbag record -a
