d173518578e2f66de12a13a4192ff96b022c3812
Jetson Leopart <curly@umich.edu>
2021-05-28 18:58:28 -0400

    update script

--- scripts/catkin_make.sh
+++ scripts/catkin_make.sh
@@ -2,6 +2,8 @@
 # git checkout minicheetah
 # cd ../..
 cd catkin_ws
-catkin_make install -DCMAKE_BUILD_TYPE=Release -DOCTOMAP_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/ -Doctomap_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/
-# catkin_make install -DCMAKE_BUILD_TYPE=Release -DOCTOMAP_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/ -Doctomap_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/
-# catkin_make install -DCMAKE_BUILD_TYPE=Release -DOCTOMAP_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/ -Doctomap_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/
+catkin_make install -DCMAKE_BUILD_TYPE=Release -DCATKIN_ENABLE_TESTING=False -DOCTOMAP_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/ -Doctomap_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/
+
+catkin_make install -DCMAKE_BUILD_TYPE=Release -DCATKIN_ENABLE_TESTING=False -DOCTOMAP_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/ -Doctomap_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/
+
+catkin_make install -DCMAKE_BUILD_TYPE=Release -DCATKIN_ENABLE_TESTING=False -DOCTOMAP_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/ -Doctomap_DIR=$PWD/../submodules/octomap/lib/cmake/octomap/
--- scripts/start_all_sensors.sh
+++ scripts/start_all_sensors.sh
@@ -1,4 +1,4 @@
 source catkin_ws/devel/setup.bash
 
-roslaunch realsense2_camera rs_camera.launch align_depth:=true enable_gyro:=true enable_accel:=true unite_imu_method:=linear_interpolation &
+roslaunch realsense2_camera rs_camera.launch align_depth:=true enable_sync:=true depth_width:=640 depth_height:=480 depth_fps:=30 color_width:=640 color_height:=480 color_fps:=30 enable_gyro:=true enable_accel:=true unite_imu_method:=linear_interpolation &
 rosrun mini-cheetah lcm_to_ros_node
